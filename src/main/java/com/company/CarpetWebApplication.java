81ece356ae5630d7ff7ea46a3d80ccce222a9f3d
hasan3552 <hasan3552fayz@gmail.com>
2022-06-13 12:41:15 +0500

    Initial commit

---
+++ src/main/java/com/company/CarpetWebApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpetWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpetWebApplication.class, args);
+	}
+
+}
